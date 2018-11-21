7a59d029afbfe97090fe9f9f6e442b54a3297540
Taylor Tu <tkt7@njit.edu>
2018-11-21 01:12:38 -0500

    Fixed navbar bugs, tracked scroll so navbar appears only on scroll up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { MaterialModule } from '@blox/material';
-import { FormsModule } from '@angular/forms';
+import { MaterialModule } from "@blox/material";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatTabsModule, MatChipsModule, MatDialogModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { AboutComponent } from './about/about.component';
-import { BrowserComponent } from './browser/browser.component';
-import { ExperienceComponent, ExperienceDialogComponent } from './experience/experience.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { ClubsComponent } from './clubs/clubs.component';
+import { AppComponent } from "./app.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { LayoutModule } from "@angular/cdk/layout";
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatIconModule,
+  MatListModule,
+  MatCardModule,
+  MatTabsModule,
+  MatChipsModule,
+  MatDialogModule
+} from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { AboutComponent } from "./about/about.component";
+import { BrowserComponent } from "./browser/browser.component";
+import {
+  ExperienceComponent,
+  ExperienceDialogComponent
+} from "./experience/experience.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ClubsComponent } from "./clubs/clubs.component";
 
 @NgModule({
   declarations: [
@@ -44,12 +58,11 @@ import { ClubsComponent } from './clubs/clubs.component';
     FlexLayoutModule,
     MatTabsModule,
     MatChipsModule,
-    MatDialogModule
+    MatDialogModule,
+    MatMenuModule
   ],
-  entryComponents: [
-    ExperienceDialogComponent
-  ],
+  entryComponents: [ExperienceDialogComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Component } from "@angular/core";
+import { fromEvent } from "rxjs";
+import { initDomAdapter } from "@angular/platform-browser/src/browser";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.css"]
 })
 export class NavbarComponent {
+  constructor() {}
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  ngOnInit() {
+    var initialPageOffset = window.pageYOffset;
+    fromEvent(document, "scroll").subscribe(() => {
+      var currentPageOffset = window.pageYOffset;
+      var el = document.getElementById("navbar");
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+      if (currentPageOffset < initialPageOffset) {
+        el.style.top = "0";
+        console.log("scroll up");
+      } else {
+        el.style.top = "-65px";
+      }
 
+      initialPageOffset = currentPageOffset;
+    });
+  }
 }
